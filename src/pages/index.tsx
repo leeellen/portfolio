82bea9a1c019e032642c8d8ccba1ba077048721d
leeellen <lee.ellen0814@gmail.com>
2023-04-25 17:32:22 +0900

    add : title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
+import Head from 'next/head';
+
 export default function Home() {
-    return <div></div>;
+    return (
+        <div>
+            <Head>
+                <title>Ellen</title>
+            </Head>
+        </div>
+    );
 }
