f6a7734b754a1f7c70d9d0a8ff9c3f169793efc0
leeellen <lee.ellen0814@gmail.com>
2023-05-19 18:16:05 +0900

    add : go to top

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -32,38 +32,37 @@ const projectList = [
 
 export default function Projects({}: Props) {
     return (
-        <div className="h-screen relative  flex flex-col text-left md:flex-row m-w-full justify-evenly mx-auto items-center z-0">
+        <div className="h-screen relative flex flex-col text-left md:flex-row m-w-full justify-evenly mx-auto items-center z-0 overflow-hidden">
             <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">Projects</h3>
 
-            <div className="relative w-full flex z-20">
-                <Swiper spaceBetween={40} slidesPerView={1}>
-                    {projectList.map((e, i) => (
-                        <SwiperSlide key={e.title}>
-                            <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
-                                <Link href={e.link} target="_blank" rel="noopener">
-                                    <motion.img
-                                        initial={{ y: -100, opacity: 0 }}
-                                        whileInView={{ y: 0, opacity: 1 }}
-                                        transition={{ duration: 1.2 }}
-                                        viewport={{ once: true }}
-                                        src={e.img.src}
-                                        className="md:mb-0 flex-shrink-0 w-96 h-96 rounded-full md:rounded-lg  object-contain cursor-pointer"
-                                    />
-                                </Link>
-                                <div className="space-y-10 px-0 md:px-10 max-w-6xl">
-                                    <h4 className="text-4xl font-semibold text-center">
-                                        <span className="underline decoration-[#fb3131]/50">
-                                            Project {i + 1} of {projectList.length} :
-                                        </span>{' '}
-                                        {e.title}
-                                    </h4>
+            <div className="relative w-full flex z-20 overflow-x-scroll overflow-y-hidden snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#fb3131]/80 ">
+                {projectList.map((e, i) => (
+                    <div
+                        key={e.link}
+                        className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen"
+                    >
+                        <Link href={e.link} target="_blank" rel="noopener">
+                            <motion.img
+                                initial={{ y: -100, opacity: 0 }}
+                                whileInView={{ y: 0, opacity: 1 }}
+                                transition={{ duration: 1.2 }}
+                                viewport={{ once: true }}
+                                src={e.img.src}
+                                className="md:mb-0 flex-shrink-0 w-96 h-96 rounded-full md:rounded-lg  object-contain cursor-pointer"
+                            />
+                        </Link>
+                        <div className="space-y-10 px-0 md:px-10 max-w-6xl">
+                            <h4 className="text-4xl font-semibold text-center">
+                                <span className="underline decoration-[#fb3131]/50">
+                                    Project {i + 1} of {projectList.length} :
+                                </span>{' '}
+                                {e.title}
+                            </h4>
 
-                                    <p className="whitespace-pre-wrap">{e.desc}</p>
-                                </div>
-                            </div>
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
+                            <p className="whitespace-pre-wrap">{e.desc}</p>
+                        </div>
+                    </div>
+                ))}
             </div>
             <div className="w-full absolute top-[30%] bg-[#fb3131]/10 left-0 h-[500px] -skew-y-12" />
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
+
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import About from '@/components/About';
@@ -7,9 +10,15 @@ import Skills from '@/components/Skills';
 import Projects from '@/components/Projects';
 import ContactMe from '@/components/ContactMe';
 
+import hero from 'public/images/hero.png';
+
 export default function Home() {
     return (
-        <div className="scroll-smooth bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+        <div
+            className="scroll-smooth bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0
+        scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#fb3131]/80 
+        "
+        >
             <Head>
                 <title>Ellen</title>
             </Head>
@@ -28,17 +37,27 @@ export default function Home() {
                 <Experience />
             </section>
 
-            <section id="skills" className="snap-center">
+            <section id="skills" className="snap-start">
                 <Skills />
             </section>
 
-            <section id="projects" className="snap-center">
+            <section id="projects" className="snap-start">
                 <Projects />
             </section>
 
             <section id="contactme" className="snap-center">
                 <ContactMe />
             </section>
+
+            <Link href="#hero">
+                <footer className="sticky bottom-5 w-full cursor-pointer">
+                    <div className="flex justify-center items-center">
+                        <div className="w-11 h-11 bg-[#fb3131] rounded-full filter grayscale hover:grayscale-0 cursor-pointer flex justify-center items-center">
+                            <Image src={hero} className="h-10 w-10 " alt="go to top button" />
+                        </div>
+                    </div>
+                </footer>
+            </Link>
         </div>
     );
 }
